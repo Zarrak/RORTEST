75c2cfc5e5ba2ad051a1d58be93cb2dfc982f460
Markus Samlenski <Zarrak@gmx.de>
2013-03-08 18:20:48 +0100

    RORTEST

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3BootstrapDeviseCancan::Application.config.secret_token = 'a6cd68af2c0c93e2f0bd4c576aab509f59b7ae839d02018caa4c8ce3cc46ea012fdc4f1dc93bbb14137bc646fb697907e9649260d02b77eed838717d20dff479'
+RORTEST::Application.config.secret_token = '9c4feae0a64f0f328dab3aa8281c5011e4094dd6977af047c252cb39f8929fb69525af6ffac16ac31d1ab0a901a5d05af6d3cb84d2c353632d65bf6955081b70'
